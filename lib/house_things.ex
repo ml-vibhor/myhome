8ee77f8e302cdc655aae276272b8635b8cc99eb3
ml.vibhor <ml.vibhor@gmail.com>
2021-02-28 19:35:58 +0530

    first commit

---
+++ lib/house_things.ex
@@ -0,0 +1,18 @@
+defmodule HouseThings do
+  @moduledoc """
+  Documentation for `HouseThings`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HouseThings.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/house_things_test.exs
@@ -0,0 +1,8 @@
+defmodule HouseThingsTest do
+  use ExUnit.Case
+  doctest HouseThings
+
+  test "greets the world" do
+    assert HouseThings.hello() == :world
+  end
+end
