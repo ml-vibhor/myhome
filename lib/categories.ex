c24fbc01a44187e811db1ba6629b4a60692d6327
ml.vibhor <ml.vibhor@gmail.com>
2021-03-01 11:09:26 +0530

    Agent topic completed

---
+++ lib/categories.ex
@@ -0,0 +1,20 @@
+defmodule Categories do
+  use Agent
+
+  def start_link(opts) do
+    Agent.start_link(fn -> %{} end, opts)
+  end
+
+  def get_cat_item(cat_agent_pid, cat_name) do
+    # &1 => current state
+    Agent.get(cat_agent_pid, &Map.get(&1, cat_name))
+  end
+
+  def set_cat_item(cat_agent_pid, cat_name, cat_value) do
+    Agent.update(cat_agent_pid, &Map.put(&1, cat_name, cat_value))
+  end
+
+  def delete_cat_item(cat_agent_pid, cat_name) do
+    Agent.get_and_update(cat_agent_pid, &Map.pop(&1, cat_name))
+  end
+end
